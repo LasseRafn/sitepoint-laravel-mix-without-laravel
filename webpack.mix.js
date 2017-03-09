e304b3bbc1c57771c1e03221cfb9dbeaba25a099
Lasse Rafn <lasserafn@gmail.com>
2017-03-09 15:40:16 +0100

    6. Ran test + fixed permissions

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const {mix} = require('laravel-mix');
 
-mix.js('js/app.js', 'public/js')
-   .sass('sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
