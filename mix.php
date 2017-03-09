53223aea507c850a4996022aac2464b08e1d349c
Lasse Rafn <lasserafn@gmail.com>
2017-03-09 16:35:47 +0100

    9. Added mix.php for mix() method

---
+++ mix.php
@@ -0,0 +1,37 @@
+<?php if (! function_exists('mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param string $path
+     * @param string $manifestDirectory
+     * @return \Illuminate\Support\HtmlString
+     *
+     * @throws \Exception
+     */
+    function mix($path, $manifestDirectory = '')
+    {
+        static $manifest;
+        $publicPath = $_SERVER[‘DOCUMENT_ROOT’] . ‘/public’; // This replaces the public_path method from Laravel.
+        if ($manifestDirectory && ! starts_with($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+        if (! $manifest) {
+            if (! file_exists($manifestPath = ($manifestDirectory.'/mix-manifest.json') )) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+        }
+        if (! starts_with($path, '/')) {
+            $path = "/{$path}";
+        }
+        if (! array_key_exists($path, $manifest)) {
+            throw new Exception(
+                "Unable to locate Mix file: {$path}. Please check your ".
+                'webpack.mix.js output paths and try again.'
+            );
+        }
+        return file_exists($publicPath . ($manifestDirectory.'/hot'))
+                    ? new Illuminate\Support\HtmlString("http://localhost:8080{$manifest[$path]}")
+                    : new Illuminate\Support\HtmlString($manifestDirectory.$manifest[$path]);
+    }
+}
