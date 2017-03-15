5ea8df88441abcbe04148e44f86a93b2c7c4415b
Lasse Rafn <lasserafn@gmail.com>
2017-03-16 00:52:10 +0100

    Hot reloading setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const {mix} = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .version()
-   .browserSync('demo.dev');
+   .browserSync();
